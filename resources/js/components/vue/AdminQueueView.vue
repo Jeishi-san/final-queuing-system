f6244bd7cb73916636d322c872af15041fb7c450
nightsheepcodes <joshicalina5@gmail.com>
2025-12-09 22:02:17 +0800

    UI Update: Responsive Design - AdminQueueView

--- resources/js/components/vue/AdminQueueView.vue
+++ resources/js/components/vue/AdminQueueView.vue
@@ -8,8 +8,6 @@
 
     import icon from '../../../assets/img/login-icon.png';
 
-    const systemName = 'IT Ops Queuing System';
-
     const showAddTicket = ref(false);
     const ticketSubmitted = ref(false);
 
@@ -58,44 +56,55 @@
 </script>
 
 <template>
-    <div class="relative overflow-hidden min-h-screen flex flex-col">
-        <header class="w-full flex items-center justify-between p-4 bg-white shadow-md">
+    <div class="relative
+                xs:flex xs:flex-col xs:items-center">
+        <header class="p-4 bg-white shadow-md
+                        xs:w-full xs:flex xs:flex-col xs:items-center
+                        md:flex-row md:justify-between md:px-8">
 
-            <div class="flex items-center justify-center">
+            <div class="
+                        xs:flex xs:flex-col xs:items-center xs:mb-2
+                        md:flex-row md:justify-between md:mb-0">
                 <!-- icon -->
-                <img :src="icon" alt="App Icon" class="w-20 h-20 ml-6 mr-3" />
+                <img :src="icon" alt="App Icon" class="w-20 h-20" />
 
                 <!-- System Title -->
-                <h2 class="text-2xl font-bold text-[#003D5B]"
-                > {{ systemName }}
-                </h2>
+                <h2 class="text-2xl font-bold text-[#003D5B]">CNX IT Ops enQ</h2>
             </div>
 
             <!-- Date and Time -->
-            <div class="flex flex-col items-center space-y-2 mr-6 text-[#003D5B]">
+            <div class="text-[#003D5B]
+                        xs:flex xs:space-x-2
+                        md:flex-col-reverse md:space-x-0 md:items-center">
                 <Clock />
                 <Date />
             </div>
         </header>
 
-        <div class="flex w-full px-10 py-14">
+        <div class="px-10 py-14 pt-7
+                    xs:w-full xs:space-y-10
+                    xl:flex xl:space-x-10 xl:space-y-0">
             <!-- LEFT SIDE -->
             <div class="flex flex-col w-full space-y-10">
                 <InProgress
                     :queueNum="inProgress[0]?.queue_number?? 'none'"
                     :ticketId="inProgress[0]?.ticket.ticket_number?? 'none'"
                     :itStaff="inProgress[0]?.assigned_user.name?? 'none'"
-                    style_div="w-[95%] h-65 bg-white rounded-3xl items-center shadow-[0_10px_50px_5px_rgba(0,0,0,0.3)]"
+                    style_div="w-[95%] h-[50%] bg-white rounded-3xl items-center justify-center shadow-[0_10px_50px_5px_rgba(0,0,0,0.3)]
+                                xs:w-full"
                     style_h3="text-3xl font-bold text-[#003D5B]"
-                    style_h1="text-8xl font-bold text-[#003D5B] my-5"
+                    style_h1="font-bold text-[#003D5B] my-5
+                                xs:text-5xl md:text-8xl"
                     style_p="text-base text-gray-700"/> <!-- In Progress Ticket Card 1 -->
                 <InProgress
                     :queueNum="inProgress[1]?.queue_number?? 'none'"
                     :ticketId="inProgress[1]?.ticket.ticket_number?? 'none'"
                     :itStaff="inProgress[1]?.assigned_user.name?? 'none'"
-                    style_div="w-[95%] h-65 bg-white rounded-3xl"
+                    style_div="w-[95%] h-[50%] bg-white rounded-3xl items-center justify-center shadow-[0_10px_50px_5px_rgba(0,0,0,0.3)]
+                                xs:w-full"
                     style_h3="text-3xl font-bold text-[#003D5B]"
-                    style_h1="text-8xl font-bold text-[#003D5B] my-5"
+                    style_h1="font-bold text-[#003D5B] my-5
+                                xs:text-5xl md:text-8xl"
                     style_p="text-base text-gray-700"/> <!-- In Progress Ticket Card 2 -->
             </div>
 
@@ -109,7 +118,9 @@
         <a href="/dashboard">
             <button
                 @click="$emit('prev_page')"
-                class="fixed bottom-5 left-5 text-white p-1 px-2 shadow-xl rounded-2xl hover:bg-[#029cda] transition"
+                class="fixed shadow-xl transition hover:bg-[#029cda]
+                        xs:bottom-1/2 xs:left-[-40px] xs:pl-11 xs:py-5 xs:pr-1 xs:bg-white xs:text-[#003D5B] xs:hover:text-white xs:rounded-[100%]
+                        xl:bottom-5 xl:left-5 xl:text-white xl:p-1 xl:px-2 xl:rounded-2xl xl:bg-transparent"
             >
                 <span class="font-bold text-xl">
                     <FontAwesomeIcon :icon="['fas', 'arrow-left-long']" />
